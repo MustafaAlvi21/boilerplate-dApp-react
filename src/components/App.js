97f63d6c1da7c2e6a4234ef561569a74da0c60f3
MustafaAlvi21 <mustafaalvi21@gmail.com>
2021-04-30 10:29:47 +0500

    2nd commit

--- src/components/App.js
+++ src/components/App.js
@@ -55,108 +55,49 @@ function App() {
     }
   }
 
-
-// }
-
-
-// class App extends Component {
-  // async componentWillMount() {
-  //   await this.loadWeb3()
-  //   await this.loadBlockchainData()
-  // }
-
-  // async loadWeb3() {
-  //   if (window.ethereum) {
-  //     window.web3 = new Web3(window.ethereum)
-  //     await window.ethereum.enable()
-  //   }
-  //   else if (window.web3) {
-  //     window.web3 = new Web3(window.web3.currentProvider)
-  //   }
-  //   else {
-  //     window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
-  //   }
-  // }
-
-  // async loadBlockchainData() {
-  //   const web3 = window.web3
-  //   // console.log(web3)
-    
-    
-  //   // Load account
-  //   const accounts = await web3.eth.getAccounts()
-  //   // console.log(accounts)
-  //   this.setState({ account: accounts[0] })
-  //   console.log(accounts[0])
-    
-    
-  //   // Network ID
-  //   const networkId = await web3.eth.net.getId()
-  //   // console.log(networkId)
-  //   const networkData = DStorage.networks[networkId]
-  //   if(networkData) {
-    
-  //     // Assign contract
-  //     const dstorage = new web3.eth.Contract(DStorage.abi, networkData.address)
-  //     // console.log(dstorage);
-  //     this.setState({ dstorage })
-  //     // Get files amount
-  //     const filesCount = await dstorage.methods.getAllUsers().call()
-  //     // const filesCount = await dstorage.methods.registerAccount("Mustafa", "0xA921A30BFED52e864D24B6277ef84E23798a4cC8").send( {from: accounts[0], gas:3000000} )
-
-  //     console.log(filesCount);
-
-  //     // Load files&sort by the newest
-  //   } else {
-  //     window.alert('DStorage contract not deployed to detected network.')
-  //   }
-  // }
-
-  
-  // render() {
-    return (
-      <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University</h1>
-                <h5>DApp Template_v1</h5>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
-              </div>
-            </main>
-          </div>
+  return (
+    <div>
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <a
+          className="navbar-brand col-sm-3 col-md-2 mr-0"
+          href="https://github.com/MustafaAlvi21/boilerplate-dApp-react"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React
+        </a>
+      </nav>
+      <div className="container-fluid mt-5">
+        <div className="row">
+          <main role="main" className="col-lg-12 d-flex text-center">
+            <div className="content mr-auto ml-auto">
+              <a
+                href="https://github.com/MustafaAlvi21/boilerplate-dApp-react"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={logo} className="App-logo" alt="logo" width="200"/>
+              </a>
+              <h1>Boilerplate</h1>
+              <h5>React + Truffle.js for Ethereum dApp</h5>
+              <p>
+                Edit <code>src/components/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://github.com/MustafaAlvi21/boilerplate-dApp-react"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                CLONE IT <u><b>NOW! </b></u>
+              </a>
+            </div>
+          </main>
         </div>
       </div>
-    );
-  // }
+    </div>
+  );
+
 }
 
 export default App;
